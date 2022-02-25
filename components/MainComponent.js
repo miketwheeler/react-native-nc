389bf0efc9f772f7266e6d6262517545a97ca3c3
miketwheeler <mikeytwheeler@gmail.com>
2022-02-25 00:27:02 -0800

    home page & slide drawer

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,32 +1,68 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Constants from 'expo-constants';
-import { Platform, View } from 'react-native';
+import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
         CampsiteInfo: { screen: CampsiteInfo }
-    }, 
+    },
     {
         initialRouteName: 'Directory',
         defaultNavigationOptions: {
-            headerStyle: {backgroundColor: '#5637DD',},
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
             headerTintColor: '#fff',
-            headerTitleStyle: {color: '#fff',}
+            headerTitleStyle: {
+                color: '#fff'
+            }
         }
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
+const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
     render() {
         return (
-            <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }} >
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+            }}>
                 <AppNavigator />
             </View>
         );
